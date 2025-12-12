bc1c9bddda32d4c29621bfd8d5ca189469c3be91
songzuo <songzuo@example.com>
2025-12-12 15:00:09 +0800

    Configure GitHub Pages deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/love/', // 设置GitHub Pages的base路径
   server: {
     port: 3000,
     proxy: {
